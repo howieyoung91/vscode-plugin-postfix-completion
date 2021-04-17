642df5d5fb59ee18572ebab1254756c08caa65aa
Howie <ishowieyoung@gmail.com>
2021-04-17 19:54:58 +0800

    0.0.5

--- src/postfix/java/VarPostfixHandler4J.ts
+++ src/postfix/java/VarPostfixHandler4J.ts
@@ -6,15 +6,22 @@ import LineTextHandleResult from "../../base/LinetextHandleResult";
 @PostfixHandler({ language: "java", label: "var" })
 class VarPostfixHandler4J extends BasePostfixHandler {
   handleLineText(lineText: string): null | LineTextHandleResult {
-    if (!lineText.match(/^(.*?)new (.+?)\(.*\)[.var]{0,4}$/)) {
+    if (!lineText.match(/^\s*new (.+?)\(.*\)[.var]{0,4}$/)) {
       return null;
     }
-    let startIndex = lineText.lastIndexOf("new");
+    let startIndex = lineText.indexOf("new");
     let endIndex = lineText.lastIndexOf(".");
     // 需要替换的文本
     let replacement = lineText.substring(startIndex, endIndex);
     // 获取类名
-    let clazz = lineText.substring(startIndex + 4, lineText.lastIndexOf("("));
+    let clazz = replacement.substring(
+      startIndex + 4,
+      lineText.lastIndexOf("(")
+    );
+    let tempIndex = clazz.indexOf("<");
+    if (tempIndex !== -1) {
+      clazz = clazz.substring(0, tempIndex);
+    }
     return {
       text: new SnippetString(
         `${clazz} \${1:${clazz.toLowerCase()}} = ${replacement};`
